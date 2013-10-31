8f1e196bdce492eba08f6243e419fd3b3d5ec142
Alexandre Kandalintsev <spam@messir.net>
2013-10-31 08:54:08 +0100

    ./arithmetic_eval.py: some fixes

--- tallentbuddy.co/arithmetic_eval.py
+++ tallentbuddy.co/arithmetic_eval.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import re
-from operator import mul, sub, add
-symap = {'+': add, '-': sub, '*': mul}
+from operator import mul, sub, add, floordiv
+symap = {'+': add, '-': sub, '*': mul, '/': floordiv}
 opmap = {sub: 10, add: 10, mul: 5, '/': 5, '(': 1, ')': 1}
 def arithmetic_eval(expr):
   syms = re.findall("(\d+|[-+*/()])", expr.replace(" ",""))
-  expr = [symap[s] if s in symap else s for s in syms]
+  #expr = [symap[s] if s in symap else s for s in syms]
   expr = [symap[s] if s in symap else int(s) if s.isnumeric() else s for s in syms]
   return calc(iter(expr))
 
@@ -16,30 +16,31 @@ def calc(syms):
     if sym == '(':
       nums.append(calc(syms))
     elif sym == ')':
-      return stack(ops, nums)
+      return calc_stack(ops, nums)
     elif sym in opmap:
-      if not ops or opmap[sym] < opmap[ops[-1]]:
+      if not ops or opmap[sym] <= opmap[ops[-1]]:
         ops.append(sym)
       else:
-        v = stack(ops, nums)
+        v = calc_stack(ops, nums)
         nums.append(v)  #TODO:
     else:
       nums.append(sym)
-  return stack(ops, nums)
+  return calc_stack(ops, nums)
 
-def stack(ops, nums):
+def calc_stack(ops, nums):
+  print(ops, nums, end=' ')
   while ops:
     op = ops.pop()
     right = nums.pop()
-    left = nums.pop()
-    print(left, op, right)
-    r = op(left, right)
-    nums.append(r)
-  return nums[0]
+    left  = nums.pop()
+    nums.append(op(left,right))
+  print(nums[-1])
+  return nums[-1]
 
 
 if __name__ == '__main__':
   #print(arithmetic_eval("( 1+2 ) *4"))
-  print(arithmetic_eval("2 + 3 * ( 1 - 4*2)"))
+  #print(arithmetic_eval("2 + 3 * ( 1 - 4*2)"))
+  print(arithmetic_eval("1 + 0 - 0  * 1 + 2 * 2 * (2)"))
 
 
