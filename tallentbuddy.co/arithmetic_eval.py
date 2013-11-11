37d248dfddc6654af5dae404fe3c4e7b0ea63f09
Alexandre Kandalintsev <spam@messir.net>
2013-11-11 17:18:29 +0100

    arithmetic_eval.py: almost done it

--- tallentbuddy.co/arithmetic_eval.py
+++ tallentbuddy.co/arithmetic_eval.py
@@ -1,46 +1,60 @@
 #!/usr/bin/env python3
-import re
 from operator import mul, sub, add, floordiv
+import re
+
 symap = {'+': add, '-': sub, '*': mul, '/': floordiv}
 opmap = {sub: 10, add: 10, mul: 5, '/': 5, '(': 1, ')': 1}
+revmap = {v:k for k, v in symap.items()}
+
 def arithmetic_eval(expr):
   syms = re.findall("(\d+|[-+*/()])", expr.replace(" ",""))
-  #expr = [symap[s] if s in symap else s for s in syms]
   expr = [symap[s] if s in symap else int(s) if s.isnumeric() else s for s in syms]
   return calc(iter(expr))
 
 def calc(syms):
   ops = []
-  nums = []
+  queue = []
   for sym in syms:
     if sym == '(':
-      nums.append(calc(syms))
+      s.append(calc_queue(queue))
     elif sym == ')':
-      return calc_stack(ops, nums)
+      return calc_queue(queue)
     elif sym in opmap:
-      if not ops or opmap[sym] <= opmap[ops[-1]]:
-        ops.append(sym)
-      else:
-        v = calc_stack(ops, nums)
-        nums.append(v)  #TODO:
+      op = opmap[sym]
+      while ops:
+        if opmap[ops[-1]] <= op:
+          queue.append(ops.pop())
+          continue
+        break
+      ops.append(op)
     else:
-      nums.append(sym)
-  return calc_stack(ops, nums)
+      queue.append(sym)
+  if ops: queue += reversed(ops)
+  return calc_queue(queue)
 
-def calc_stack(ops, nums):
-  print(ops, nums, end=' ')
-  while ops:
-    op = ops.pop()
-    right = nums.pop()
-    left  = nums.pop()
-    nums.append(op(left,right))
-  print(nums[-1])
-  return nums[-1]
+def calc_queue(queue):
+  print(queue)
+  stack = []
+  while len(queue):
+    sym = queue.pop(0)
+    if isinstance(sym, int):
+      stack.append(sym)
+    else:
+      right = stack.pop()
+      left  = stack.pop()
+      r = sym(left,right)
+      print(left, revmap[sym], right, '=', r)
+      stack.append(r)
+  return r
 
 
+
+#expr = [symap[s] if s in symap else s for s in syms]
 if __name__ == '__main__':
   #print(arithmetic_eval("( 1+2 ) *4"))
   #print(arithmetic_eval("2 + 3 * ( 1 - 4*2)"))
-  print(arithmetic_eval("1 + 0 - 0  * 1 + 2 * 2 * (2)"))
-
+  #assert arithmetic_eval("1 + 0 - 0  * 1 + 2 * 2 * (2)") == 9
+  assert arithmetic_eval("1 - 1 - 1") == -1
+  assert arithmetic_eval("1 - 2 * 3") == -5
+  #arithmetic_eval("1-2*3")
 
