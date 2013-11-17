b41e2e4a21dbbca702944a911eea1cf12ab35d70
Alexandre Kandalintsev <spam@messir.net>
2013-11-17 13:23:08 +0100

    improved paco.py

--- tallentbuddy.co/paco.py
+++ tallentbuddy.co/paco.py
@@ -11,26 +11,25 @@ def paco(data, base):
     return tuple(map(sum, zip(pos, inc)))
 
   def trace(path):
-    return sum(themap[x][y] for x,y in path[-X:])
+    return sum(themap[x][y] for x,y in path)
 
   def visit(pos=pos, path=()):
     if pos in path: return []
     path = path + (pos,)
-    if pos == dst:
-      return [path]
+    if pos == dst: return [path]  # we reached destination
     x,y = pos
     if  x not in range(N) \
      or y not in range(M) \
-     or themap[x][y] >= K:
+     or themap[x][y] >= K \
+     or trace(path[-X:]) >= thr:
       return []
     r = visit(move(pos,(1,0)), path) \
          + visit(move(pos,(-1,0)), path) \
          + visit(move(pos,(0,1)), path) \
          + visit(move(pos,(0,-1)), path)
     return [min(r, key=lambda l: len(l))] if r else []
-  path = visit()[0]
-  [print(m) for m in themap]
-  print(path)
+  path = visit()[0] # [0]
+  #[print(m) for m in themap]
   print(len(path)-1, trace(path))
 
 
